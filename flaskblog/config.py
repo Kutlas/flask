832e7cb9c56e0fb1aa13c16b64f78e3429cc679f
Farbod Raubetean <rabetian@gmail.com>
2022-08-04 00:49:00 +0200

    Fifth Commit

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = '29c25041af20120ee554185739fe6abc'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
